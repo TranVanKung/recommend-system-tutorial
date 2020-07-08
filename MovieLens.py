1e8f4a5649c63095ac3a8752a6f4e5af52037554
tranvancong <trancongtr1071997@gmail.com>
2020-07-08 22:52:56 +0700

    a recommend engine framework

--- MovieLens.py
+++ MovieLens.py
@@ -13,11 +13,12 @@ import numpy as np
 class MovieLens:
     movieID_to_name = {}
     name_to_movieID = {}
-    ratingsPath = '../ml-latest-small/ratings.csv'
-    moviesPath = '../ml-latest-small/movies.csv'
+    ratingsPath = './ml-latest-small/ratings.csv'
+    moviesPath = './ml-latest-small/movies.csv'
 
     def loadMovieLensLatestSmall(self):
         # Look for files relative to the directory we are running from
+        os.chdir(os.path.dirname(sys.argv[0]))
 
         ratingsDataset = 0
         self.movieID_to_name = {}
